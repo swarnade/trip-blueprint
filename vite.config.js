e83e46dacdf8ba0179d21c692b4b0fde4d3b24fb
Swarnadeep Saha Poddar <code.isspoddar@gmail.com>
2024-07-29 22:27:45 +0530

    Maps Updated

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,21 @@
 import dotenv from 'dotenv';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import path from 'path';
 dotenv.config();
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   define: {
     'process.env': process.env
+  },
+  resolve: {
+    alias: {
+      '@react-google-maps/api': path.resolve(__dirname, 'node_modules/@react-google-maps/api'),
+    },
+  },
+  optimizeDeps: {
+    include: ['@react-google-maps/api'],
   },
   build: {
     rollupOptions: {
