1fd1b92f23d64b1ab583720b2b9273cf7a05e13f
Swarnadeep Saha Poddar <code.isspoddar@gmail.com>
2024-08-04 20:30:03 +0530

    Readme Updated

--- src/Pages/Display.jsx
+++ src/Pages/Display.jsx
@@ -11,7 +11,7 @@ export default function Display() {
   return (
     <>
       <div className=" w-screen h-screen text-white bg-black">
-        <div className=" w-screen h-1/6 text-black text-5xl text-center">Trip Blueprint</div>
+        <div className=" w-screen h-1/6 text-white text-5xl text-center Card-Title">Trip Blueprint</div>
         <div className=" w-screen h-5/6 flex flex-col-reverse md:flex-row">
           <div className="w-screen md:w-1/2 h-2/3 md:h-full  overflow-y-scroll no-scrollbar"><Plans place={place} date={date} person={person} /></div>
           <div className="w-screen md:w-1/2 h-1/3 md:h-full  "><Maps destination={place} /></div>
