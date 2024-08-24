0f941c200ea2f94fcfce951365ae9870072c8600
Swarnadeep Saha Poddar <code.isspoddar@gmail.com>
2024-08-24 11:44:08 +0530

    History Stored in LocalStoreage

--- src/Components/Histrory.jsx
+++ src/Components/Histrory.jsx
@@ -1,13 +1,12 @@
 import React, { useEffect, useState } from 'react'
-
 export default function Histrory(props) {
 
     useEffect(() => {
-        const data = [{
+        const data = {
             Place: props.place,
             date: props.date,
             Person: props.person
-        }]
+        }
         var history = localStorage.getItem('history')
         if (history) {
             var history_json = JSON.parse(history)
@@ -15,10 +14,8 @@ export default function Histrory(props) {
             localStorage.setItem('history', JSON.stringify(history_json))
         }
         else {
-            localStorage.setItem('history', JSON.stringify(data))
+            localStorage.setItem('history', JSON.stringify([data]))
         }
-
-        console.log(history_json)
     }, [])
     return (
         <>
